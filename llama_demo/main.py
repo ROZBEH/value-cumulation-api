16bafbd910c0abe384e74e594b632b5a378840d8
rozbeh <rouzbeh.asghari@gmail.com>
2023-05-22 22:08:02 -0700

    update main + remove unused files

--- llama_demo/main.py
+++ llama_demo/main.py
@@ -1,15 +1,27 @@
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
-from dotenv import load_dotenv
+import os
+from utility import index_txt_doc, index_html_doc, index_sec_url
 
-load_dotenv()
-
-documents = SimpleDirectoryReader(
-    "/Users/rouzbeh/value-cumulation/value-cumulation-api/llama_index/examples/paul_graham_essay/data"
-).load_data()
-index = GPTVectorStoreIndex.from_documents(documents)
-
+index = index_sec_url()
 query_engine = index.as_query_engine()
+# query = "Which company this report is about?"
+# results = query_engine.query(query)
+# print(results)
+# query = "What is the document type?"
+# results = query_engine.query(query)
+# print(results)
+query = "What is the product that Apple makes the most money out of?"
+results = query_engine.query(query)
+print(results)
+query = "How about the least?"
+results = query_engine.query(query)
+print(results)
+query = "What is the product that Apple makes the least money out of?"
+results = query_engine.query(query)
+print(results)
+# query = "How much apple executives make?"
+# results = query_engine.query(query)
+# print(results)
 
-query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up?")
-print(response)
+query = "Who is on the BOD?"
+results = query_engine.query(query)
+print(results)
