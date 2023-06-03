334356d866db23592527f1bcfbd65eec2b1cad0f
rozbeh <rouzbeh.asghari@gmail.com>
2023-06-02 22:19:25 -0700

    fix gitignore

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request, abort
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    request_api_key = request.headers.get("VALUE_CUMULATION_API_KEY")
+    true_api_key = os.environ.get("API_KEY")
+    if request_api_key != true_api_key:
+        abort(401)  # Unauthorized
+    return "Hello World, you are authorized!"
