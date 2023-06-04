4e2fb458a20512fb709d4f335d9ea565806e16bd
rozbeh <rouzbeh.asghari@gmail.com>
2023-06-03 20:39:08 -0700

    add a welcome link

--- app.py
+++ app.py
@@ -6,8 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    ############################## Athenticate the user ##############################
     request_api_key = request.headers.get("VALUE_CUMULATION_API_KEY")
     true_api_key = os.environ.get("API_KEY")
     if request_api_key != true_api_key:
         abort(401)  # Unauthorized
+    ##################################################################################
     return "Hello World, you are authorized!"
+
+
+@app.route("/greetings")
+def greeting(username):
+    ############################## Athenticate the user ##############################
+    request_api_key = request.headers.get("VALUE_CUMULATION_API_KEY")
+    true_api_key = os.environ.get("API_KEY")
+    if request_api_key != true_api_key:
+        abort(401)  # Unauthorized
+    ##################################################################################
+
+    return "Hello there and welcome!"
