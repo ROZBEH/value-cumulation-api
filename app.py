18f19f83a5475bb61f0ce633f095fdfa0adbd834
rozbeh <rouzbeh.asghari@gmail.com>
2023-06-03 23:12:18 -0700

    add support for running queries on companies

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 import os
+from llama_demo.utility import index_sec_url
 
 app = Flask(__name__)
 
@@ -25,3 +26,20 @@ def greeting():
     ##################################################################################
 
     return "Hello there and welcome!"
+
+
+@app.route("/answer_financial_queries")
+def answer_financial_queries():
+    ############################## Athenticate the user ##############################
+    request_api_key = request.headers.get("VALUE_CUMULATION_API_KEY")
+    true_api_key = os.environ.get("API_KEY")
+    if request_api_key != true_api_key:
+        abort(401)
+    ##################################################################################
+    query = request.args.get("query")
+    ticker = request.args.get("ticker")
+    index = index_sec_url(ticker=ticker)
+    query_engine = index.as_query_engine()
+    results = query_engine.query(query)
+
+    return jsonify(results)
