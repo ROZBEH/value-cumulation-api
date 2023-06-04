2df1c2ff11c1251a3956159998ecb0c09b026333
rozbeh <rouzbeh.asghari@gmail.com>
2023-06-03 23:11:49 -0700

    clean up the utility functions + add init + indexList

---
+++ llama_demo/__init__.py
@@ -0,0 +1,1 @@
+from llama_demo.utility import index_txt_doc, index_html_doc, index_sec_url
--- llama_demo/utility.py
+++ llama_demo/utility.py
@@ -6,7 +6,6 @@ from llama_index import (
     ServiceContext,
     GPTVectorStoreIndex,
     SimpleDirectoryReader,
-    StringIterableReader,
     StorageContext,
     load_index_from_storage,
     LLMPredictor,
@@ -19,6 +18,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def index_txt_doc(path) -> GPTVectorStoreIndex:
@@ -60,7 +61,7 @@ def index_html_doc(path) -> GPTVectorStoreIndex:
     )
     if not Path("./storage").is_dir():
         print("creating index")
-        UnstructuredReader = download_loader("UnstructuredReader", refresh_cache=True)
+        UnstructuredReader = download_loader("UnstructuredReader")
         loader = UnstructuredReader()
         document = loader.load_data(file=Path(path), split_documents=False)
         index = GPTVectorStoreIndex.from_documents(
@@ -80,18 +81,14 @@ def index_html_doc(path) -> GPTVectorStoreIndex:
 # write the code and have the default value be the year previous to the current year
 
 
-def index_sec_url(report_type="10-K", ticker="AAPL", year=None) -> GPTVectorStoreIndex:
-    """
-    Reads the contents of an html file and creates a GPTVectorStoreIndex from it.
+def is_ticker_in_file(ticker, filename="indexList.txt"):
+    filename = os.path.join(current_dir, filename)
+    with open(filename, "r") as file:
+        tickers = file.read().splitlines()
+    return ticker in tickers
 
-    Args:
-        report_type (str): Type of report to download
-        ticker (str): Ticker of the company to download
 
-    Returns:
-        index (GPTVectorStoreIndex): The GPTVectorStoreIndex created from the text file.
-
-    """
+def fetch_sec_report(report_type="10-K", ticker="AAPL", year=None):
     if year is None:
         year = (
             datetime.datetime.now().year - 2
@@ -116,12 +113,32 @@ def index_sec_url(report_type="10-K", ticker="AAPL", year=None) -> GPTVectorStor
     filings = queryApi.get_filings(query)
     sec_url = filings["filings"][0]["linkToFilingDetails"]
     filing = renderApi.get_filing(sec_url)
-    html_file = "tmp_" + ticker + ".html"
+    html_file = os.path.join(current_dir, "tmp_" + ticker + ".html")
     with open(html_file, "w") as f:
         f.write(filing)
+    index_list_file = os.path.join(current_dir, "indexList.txt")
+    with open(index_list_file, "a") as f:
+        f.write(ticker + "\n")
+    return html_file
+
+
+def index_sec_url(report_type="10-K", ticker="AAPL", year=None) -> GPTVectorStoreIndex:
+    """
+    Reads the contents of an html file and creates a GPTVectorStoreIndex from it.
+
+    Args:
+        report_type (str): Type of report to download
+        ticker (str): Ticker of the company to download
+
+    Returns:
+        index (GPTVectorStoreIndex): The GPTVectorStoreIndex created from the text file.
+
+    """
+    html_file = None
+    if not is_ticker_in_file(ticker):
+        html_file = fetch_sec_report(report_type, ticker, year)
 
     llm_predictor = LLMPredictor(
-        # gpt-3.5-turbo
         llm=ChatOpenAI(model_name="gpt-3.5-turbo", max_tokens=512, temperature=0.1)
         # llm=OpenAIChat(model_name="gpt-4", max_tokens=512, temperature=0.1)
     )
@@ -129,30 +146,41 @@ def index_sec_url(report_type="10-K", ticker="AAPL", year=None) -> GPTVectorStor
     service_context = ServiceContext.from_defaults(
         llm_predictor=llm_predictor, chunk_size_limit=512
     )
-    if not Path("./storage").is_dir():
-        print("creating index")
-        # reading the json html file approach
-        UnstructuredReader = download_loader("UnstructuredReader", refresh_cache=True)
-        loader = UnstructuredReader()
-        document = loader.load_data(file=Path(html_file), split_documents=False)
-        # read the txt file approach
-        index = GPTVectorStoreIndex.from_documents(
-            document, service_context=service_context
-        )
-        index.storage_context.persist()
+
+    storage_path = os.path.join(current_dir, "storage")
+    if html_file:
+        if Path(storage_path).is_dir():
+            storage_context = StorageContext.from_defaults(persist_dir=storage_path)
+            print("Index found. Loading ...")
+            print(f"Adding {ticker} to the index")
+            UnstructuredReader = download_loader("UnstructuredReader")
+            loader = UnstructuredReader()
+            document = loader.load_data(file=Path(html_file), split_documents=False)
+            # load index
+            index = load_index_from_storage(
+                storage_context, service_context=service_context
+            )
+
+            index.insert(document[0])
+            index.storage_context.persist(persist_dir=storage_path)
+        else:
+            print("No index Found, creating index...")
+            # reading the json html file approach
+            UnstructuredReader = download_loader("UnstructuredReader")
+            loader = UnstructuredReader()
+            document = loader.load_data(file=Path(html_file), split_documents=False)
+            # read the txt file approach
+            index = GPTVectorStoreIndex.from_documents(
+                document,
+                service_context=service_context,
+            )
+            index.storage_context.persist(persist_dir=storage_path)
     else:
-        print("loading index")
-        # Add intel to the index
-        UnstructuredReader = download_loader("UnstructuredReader", refresh_cache=False)
-        loader = UnstructuredReader()
-        document = loader.load_data(file=Path(html_file), split_documents=False)
-        # load index
-        storage_context = StorageContext.from_defaults(persist_dir="./storage")
+        storage_context = StorageContext.from_defaults(persist_dir=storage_path)
+        print(f"{ticker} is already in the index. Loading ...")
+        UnstructuredReader = download_loader("UnstructuredReader")
         index = load_index_from_storage(
             storage_context, service_context=service_context
         )
-
-        index.insert(document[0])
-        index.storage_context.persist()
 
     return index
